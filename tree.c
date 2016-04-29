5629559b050f8c4c1a7c2694367de3793be34f8c
Mark Rampton <mrampton@gmail.com>
2016-04-28 20:49:56 -0400

    Time values no longer appear negative at times

--- tree.c
+++ tree.c
@@ -372,6 +372,6 @@ void cleanup_index(Tree* tree) {
 double time_difference(struct timespec start, struct timespec finish) {
 	double elapsed;
 	elapsed = (finish.tv_sec - start.tv_sec);
-	elapsed = (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+	elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
 	return elapsed;
 }
